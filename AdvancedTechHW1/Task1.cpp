864b581cd47eb35f8d1cd1ce08018843cf85a48e
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2024-02-23 18:28:28 +0600

    HW7 done for Arrays course from GeekBrains

---
+++ AdvancedTechHW1/Task1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include "lib.h"
+
+
+using namespace std;
+
+
+void task1() { // 36 366 98 989 LL
+
+	ifstream f;
+	string path = "C:\\Users\\langr\\source\\C_plus_plus_repo\\ConsoleApplication1\\AdvancedTechHW1\\words.txt";
+	f.open(path); // 
+
+	int wordCounter = 0;
+	string word;
+
+	cout << "Please, enter the word: " << endl;	
+	cin >> word;
+
+	string currentWord;
+	while(!f.eof()) {
+	
+		f >> currentWord;
+
+		if (currentWord == word) wordCounter++;		
+
+		cout << "Current word: " << currentWord << ", counter: " << wordCounter << endl;
+
+		Sleep(100);
+	}
+
+	cout << "The word \"" << word << "\" repeats " << wordCounter << " times in the text given..." << endl;
+	f.close();
+}
---
+++ AdvancedTechHW1/Task2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include "lib.h"
+
+
+using namespace std;
+
+
+void task2() {
+
+	const int length = 256; // const of string length...
+
+	ifstream f;
+	string path = "C:\\Users\\langr\\source\\C_plus_plus_repo\\ConsoleApplication1\\AdvancedTechHW1\\words.txt";
+	f.open(path);
+
+	string word;
+	char* symbols = new char[length];
+	
+	while (!f.eof()) {
+	
+		f.getline(symbols, length);
+
+		cout << symbols << endl;
+	}
+	
+	f.close();
+}
