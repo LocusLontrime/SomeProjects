512a0637a794301824da40fd75fa1ac0ba9084b0
DmitryLantz <99312919+DmitryLantz@users.noreply.github.com>
2024-03-12 01:09:05 +0600

    fixes...

--- AdvancedTechHW8/Source.cpp
+++ AdvancedTechHW8/Source.cpp
@@ -2,10 +2,11 @@
 //
 
 #include <iostream>
+#include "lib.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    task1();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
---
+++ AdvancedTechHW8/Task1.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include "lib.h"
+#include <string>
+#include <vector>
+#include <map>
+#include <random>
+
+
+using namespace std;
+
+
+enum Condition {
+	ON_PAUSE,
+	PLAYING,
+	SILENCE
+};
+
+
+class Track {
+
+	public:
+		string name;
+		tm date;
+		int duration;
+
+
+};
+
+
+class AudioPlayer {
+
+	private:
+
+		vector<Track> tracks;
+
+		map<string, Track> tracksMap;
+
+		Track track_;
+
+		Condition condition;
+
+		AudioPlayer(): condition(Condition::SILENCE) {
+		
+			// some logic of tracks loading from file or etc... 
+
+		}
+
+		AudioPlayer(int val) {
+		
+
+		
+		}
+
+	public:
+
+		void play() {
+
+			string name;
+		
+			cout << "Please, choose the song you would like to listen to..." << endl;
+
+			cin >> name;
+
+			if (tracksMap.count(name)) {
+			
+				if (condition == Condition::SILENCE) {
+				
+					track_ = tracksMap[name];
+
+					condition = Condition::PLAYING;
+				
+					cout <<"PLAYING: " << track_.name << track_.duration << track_.date.tm_year
+						<< track_.date.tm_mon << track_.date.tm_mday << endl;
+				}
+
+			}
+			else {
+			
+				cout << "Sorry, but there is no such track in library..." << endl;
+
+			}
+		}
+
+		void pause() {
+			
+			if (condition == Condition::PLAYING) {
+
+				condition = Condition::ON_PAUSE;
+			
+				cout << "ON PAUSE!" << endl;
+
+			}
+			else if (condition == Condition::ON_PAUSE) {
+
+				condition = Condition::PLAYING;
+			
+				cout << "Continue PLAYING...";
+			}
+		}
+
+		void next() {
+		
+			condition = Condition::PLAYING;
+
+			track_ = tracks[rand() % tracks.size()];
+
+			cout << "PLAYING NEXT RANDOM SONG: " << track_.name << track_.duration << track_.date.tm_year
+				<< track_.date.tm_mon << track_.date.tm_mday << endl;
+		}
+
+		void stop() {
+		
+			if (condition != Condition::SILENCE) {
+			
+				condition = Condition::SILENCE;
+
+				cout << "SONG HAS BEEN STOPPED..." << endl;
+			}
+
+		}
+
+		void exit() {
+		
+			cout << "" << endl;
+
+		}
+
+};
+
+
+void task1() {
+
+
+
+}
